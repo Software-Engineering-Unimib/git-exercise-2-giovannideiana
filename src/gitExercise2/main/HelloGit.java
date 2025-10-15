59dc218bda625066def4238a506f7e01c108a523
Giovanni Deiana <g.deiana1@campus.unimib.it>
2025-10-15 11:17:08 +0200

    aggiunta nuova stampa0

--- src/gitExercise2/main/HelloGit.java
+++ src/gitExercise2/main/HelloGit.java
@@ -3,6 +3,6 @@ package gitExercise2.main;
 public class HelloGit {
 	public static void main (String [] args) {
 		System.out.println("hello git!!!");
-		System.out.println("prova di stampa");
+		System.out.println("stampa secondo utente");
 	}
 }
